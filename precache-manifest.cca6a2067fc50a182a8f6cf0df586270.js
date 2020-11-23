18d7847dcc9826c5bef73992af79d2a129b886a8
Ekaabar <kaabar.eya@gmail.com>
2020-11-23 03:50:59 +0100

    Updates

---
+++ precache-manifest.cca6a2067fc50a182a8f6cf0df586270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f008ead376d1b505666",
+    "url": "/ToDoList/static/css/main.e1159c60.chunk.css"
+  },
+  {
+    "revision": "8f008ead376d1b505666",
+    "url": "/ToDoList/static/js/main.8f008ead.chunk.js"
+  },
+  {
+    "revision": "ca9d33dca053b8c7bab6",
+    "url": "/ToDoList/static/js/runtime~main.ca9d33dc.js"
+  },
+  {
+    "revision": "cb4ce784d0fa9b80a107",
+    "url": "/ToDoList/static/css/2.6872a861.chunk.css"
+  },
+  {
+    "revision": "cb4ce784d0fa9b80a107",
+    "url": "/ToDoList/static/js/2.cb4ce784.chunk.js"
+  },
+  {
+    "revision": "a31756023043a86e8da76705393ae05b",
+    "url": "/ToDoList/index.html"
+  }
+];
